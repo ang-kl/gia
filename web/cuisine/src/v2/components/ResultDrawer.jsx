24983ceaa4e11cc8f5521d5996495059244908a3
angkl <adrian.kl.ang@gmail.com>
2026-06-23 14:25:14 +0800

    v0.62.293: restore + pass nearby strip props (single + combo)

--- web/cuisine/src/v2/components/ResultDrawer.jsx
+++ web/cuisine/src/v2/components/ResultDrawer.jsx
@@ -17,7 +17,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import ResultCard from './ResultCard.jsx';
 import { useLocale } from '../lib/i18n.js';
 
-export default function ResultDrawer({ venues, focusedPlaceId, onSelect, specialMode = null, hasFilters = false, composerOpen = false }) {
+export default function ResultDrawer({ venues, focusedPlaceId, onSelect, specialMode = null, hasFilters = false, composerOpen = false, nearbyLabel = null, nearbyAccent = null, nearbyStrips = null }) {
   const [lang] = useLocale();
   const trackRef = useRef(null);
   const list = Array.isArray(venues) ? venues : [];
@@ -158,6 +158,9 @@ export default function ResultDrawer({ venues, focusedPlaceId, onSelect, special
               specialMode={specialMode}
               horizontal
               autoExpandFocus={false}
+              nearbyLabel={nearbyLabel}
+              nearbyAccent={nearbyAccent}
+              nearbyStrips={nearbyStrips}
             />
           </div>
         ))}
